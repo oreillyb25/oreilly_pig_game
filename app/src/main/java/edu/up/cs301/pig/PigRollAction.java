250a7a5ee71e257b83d3104202260d67db56b49e
brigi <oreillyb25@up.edu>
2023-03-21 16:47:45 -0700

    pig hold and pig roll for checkpoint 1!

--- app/src/main/java/edu/up/cs301/pig/PigRollAction.java
+++ app/src/main/java/edu/up/cs301/pig/PigRollAction.java
@@ -1,4 +1,15 @@
 package edu.up.cs301.pig;
 
-public class PigRollAction {
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+public class PigRollAction extends GameAction {
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public PigRollAction(GamePlayer player) {
+        super(player);
+    }
 }
