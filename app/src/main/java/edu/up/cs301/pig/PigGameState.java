dbc5ce20b1793a1da88b57ca58b745adcfdc66d0
brigi <oreillyb25@up.edu>
2023-03-21 16:40:19 -0700

    instance vars and normal constructor

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -1,9 +1,26 @@
 package edu.up.cs301.pig;
 
+import java.util.Random;
+
 import edu.up.cs301.game.infoMsg.GameState;
 
 public class PigGameState extends GameState {
     int playerId;
     int player_zero_score;
     int player_one_score;
+    int curr_run_total;
+    int curr_value_die;
+    Random rand = new Random();
+
+    public PigGameState(){
+        //state as game begins
+        playerId = 0;
+        player_zero_score = 0;
+        player_one_score = 0;
+        curr_run_total = 0;
+        curr_value_die = rand.nextInt(6)+1;
+    }
+    //public GameState(int initId, int initPlayerZero, int initPlayerOne, int initTotal, int initCurr){
+
+    //}
 }
