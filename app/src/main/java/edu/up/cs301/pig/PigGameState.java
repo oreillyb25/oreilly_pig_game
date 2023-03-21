4b78128053cbafee5821f58ff4bc0df9d7b49274
brigi <oreillyb25@up.edu>
2023-03-21 16:35:46 -0700

    piggamestate

---
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -0,0 +1,9 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.infoMsg.GameState;
+
+public class PigGameState extends GameState {
+    int playerId;
+    int player_zero_score;
+    int player_one_score;
+}
