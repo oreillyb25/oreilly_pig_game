2ac19b5de9f5cb23e2c3bd423a0c77ffb6be401c
leeah <kenny26@up.edu>
2023-03-21 16:40:47 -0700

    first 3 getters

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -6,4 +6,18 @@ public class PigGameState extends GameState {
     int playerId;
     int player_zero_score;
     int player_one_score;
+
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public int getPlayerZeroScore() {
+        return player_zero_score;
+    }
+
+    public int getPlayerOneScore() {
+        return player_one_score;
+    }
+
 }
